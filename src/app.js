a994f8caf29be2a3eb1b72ff4f6cfacd9075b61b
shubham <shubsharma0710@gmail.com>
2025-04-20 12:31:16 +0530

    ad

--- src/app.js
+++ src/app.js
@@ -19,11 +19,11 @@ app.use(
 
 app.use(express.json({ limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit:"16kb"}));
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, "frontends")));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "frontends", "index.html"));
 });
 // import router to handle routes 
 import userRouter from "../src/routes/user.routes.js";
